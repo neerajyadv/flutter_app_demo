98223e65167f5b6c19a095822c6d545a2e085dfc
NeerYadav <nysilvalining@outlook.com>
2018-06-17 19:30:52 +0530

    Updated from Container to a card widget with container widget on top using Stack.

--- lib/sliding_windows.dart
+++ lib/sliding_windows.dart
@@ -9,16 +9,85 @@ class Sliders extends StatelessWidget{
       height: 135.0,
         child: new PageView(
             children: [
-              new Container(
-                child: new Image.asset('assets/images/saloon.jpg',
-                fit: BoxFit.cover,),
+              new Stack(
+                alignment: Alignment(0.7,0.7),
+                children: <Widget>[
+                  new Container(
+                    child: new Image.asset('assets/images/saloon.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  width: double.infinity,
+                  ),
+                  new Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: new BoxDecoration(
+                      color: Color.fromARGB(
+                        90,71,150,236
+                      ),
+                    ),
+                    child: new Text(
+                      "Now 76% Off on Grooming",
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87
+                      ),
+                    ),
+                  )
+                ],
               ),
-              new Container(
-                  child: new Image.asset('assets/images/sale.jpg',
-                      fit: BoxFit.fill)),
-              new Container(
-                child: new Image.asset('assets/images/loreal.jpg',
-                    fit: BoxFit.fill),)
+              new Stack(
+                alignment: Alignment(0.7,0.7),
+                children: <Widget>[
+                  new Container(
+                      child: new Image.asset('assets/images/sale.jpg',
+                          fit: BoxFit.fill),
+                    width: double.infinity,
+                  ),
+                  new Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: new BoxDecoration(
+                      color: Color.fromARGB(
+                          90,71,150,236
+                      ),
+                    ),
+                    child: new Text(
+                      "Get your manicure now!! 15% off",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              new Stack(
+                alignment: Alignment(0.7,0.7),
+                children: <Widget>[
+                  new Container(
+                    child: new Image.asset('assets/images/loreal.jpg',
+                        fit: BoxFit.fill),
+                    width: double.infinity,
+                  ),
+                  new Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: new BoxDecoration(
+                      color: Color.fromARGB(
+                          90,71,150,236
+                      ),
+                    ),
+                    child: new Text(
+                      "Get Loreal Products at 50% off!!",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ]
         )
     );
